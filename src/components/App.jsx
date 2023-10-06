43679820b5d7ca463a9234608c7c46b10f2683d3
nicksolony <nicksolony@gmail.com>
2023-10-05 21:02:56 -0400

    refactored app.jsx to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,49 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Section } from "./Section/Section";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Statistics } from "./Statistics/Statistics";
 
 
-export class App extends Component {
+export const App = () => {
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+  const [reviews, setReviews] = useState({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+  });
+ 
 
-  onLeaveFeedback = (option) => {
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // };
+
+  let onLeaveFeedback = (option) => {
     
     let id = option.target.innerHTML;
     
-    this.setState(prevState => {
-      return {
-        ...prevState,
-        [id]: prevState[id] + 1
-      };
-    });
+    setReviews(prevState => {
+        return {
+          ...prevState,
+          [id]: prevState[id] + 1
+        };
+      });
   };
   
-
-  render() {
-    
-    let options = Object.keys(this.state);
-    let total = Object.values(this.state).reduce((a, b) => a + b, 0);
-    let positivePercentage = this.state.good ? this.state.good / total * 100 : 0;
+    let options = Object.keys(reviews);
+    let total = Object.values(reviews).reduce((a, b) => a + b, 0);
+    let positivePercentage = reviews.good ? reviews.good / total * 100 : 0;
     
     return (
       <>
         <Section title="Please leave feedback" >
-        <FeedbackOptions options={options} onLeaveFeedback={this.onLeaveFeedback}>
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback}>
 
         </FeedbackOptions>
-        <Statistics options={options} total={total} positivePercentage={positivePercentage} state={this.state}>
+        <Statistics options={options} total={total} positivePercentage={positivePercentage} state={reviews}>
         </Statistics>
         </Section>
       </>  
     )
   };
-};
